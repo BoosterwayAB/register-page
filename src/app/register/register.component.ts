8e5fc296f01358af0713fd4cc0233f483f99082d
Sebastian <sebastian@boosterway.com>
2019-07-05 13:59:27 +0200

    First

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,96 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { finalize } from 'rxjs/operators';
+
+import { environment } from '@env/environment';
+import {
+  Logger,
+  I18nService,
+  AuthenticationService,
+  untilDestroyed
+} from '@app/core';
+
+const log = new Logger('Register');
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit, OnDestroy {
+  version: string = environment.version;
+  error: string | undefined;
+  registerForm!: FormGroup;
+  isLoading = false;
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder,
+    private i18nService: I18nService,
+    private authenticationService: AuthenticationService
+  ) {
+    this.createForm();
+  }
+
+  ngOnInit() {}
+
+  ngOnDestroy() {}
+
+  register() {
+    console.log('register');
+    // this.isLoading = true;
+    // const login$ = this.authenticationService.login(this.loginForm.value);
+    // login$
+    //   .pipe(
+    //     finalize(() => {
+    //       this.loginForm.markAsPristine();
+    //       this.isLoading = false;
+    //     }),
+    //     untilDestroyed(this)
+    //   )
+    //   .subscribe(
+    //     credentials => {
+    //       log.debug(`${credentials.username} successfully logged in`);
+    //       this.router.navigate(
+    //         [this.route.snapshot.queryParams.redirect || '/'],
+    //         { replaceUrl: true }
+    //       );
+    //     },
+    //     error => {
+    //       log.debug(`Login error: ${error}`);
+    //       this.error = error;
+    //     }
+    //   );
+  }
+
+  toLogin() {
+    this.isLoading = true;
+
+    this.router.navigate(['/login'], { replaceUrl: true });
+
+    this.isLoading = false;
+  }
+
+  setLanguage(language: string) {
+    this.i18nService.language = language;
+  }
+
+  get currentLanguage(): string {
+    return this.i18nService.language;
+  }
+
+  get languages(): string[] {
+    return this.i18nService.supportedLanguages;
+  }
+
+  private createForm() {
+    this.registerForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      repeatpassword: ['', Validators.required],
+      remember: true
+    });
+  }
+}
